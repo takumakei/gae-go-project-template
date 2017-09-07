b714f1e347b4947ec5000a84e3f13f6dab65cf77
takumakei <takumakei@gmail.com>
2017-09-07 16:40:29 +0900

    fix: lint - exported function RequestHandler should have comment or be unexported

--- src/helloworld/helloworld.go
+++ src/helloworld/helloworld.go
@@ -12,6 +12,7 @@ import (
 
 var c = cache.New(1*time.Minute, 2*time.Minute)
 
+// RequestHandler accepts a request and returns a result
 func RequestHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	log.Infof(ctx, "request url: "+r.URL.String())
 	c.Set("key", "200", cache.DefaultExpiration)
