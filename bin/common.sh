cfd9550c23587715c02f2aeda6f0f8baf14d8495
takumakei <takumakei@gmail.com>
2017-09-10 23:45:52 +0900

    use printf instead of echo -e for bash 3.x

--- bin/common.sh
+++ bin/common.sh
@@ -3,11 +3,11 @@
 set -eu
 
 run() {
-  echo -e "\e[1m==> $*\e[0m"
+  printf "\e[1m==> %s\e[0m\n" "$*"
   eval "$@"
 }
 
 err() {
-  echo -e "\e[1;38;5;255;48;5;9mError: $*\e[0m"
+  printf "\e[1;38;5;255;48;5;9mError: %s\e[0m\n" "$*"
   exit 1
 }
